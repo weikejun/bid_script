7dc7e52cf8ca7a650f857f4376945c6a6717176b
jim <jim.dev@qq.com>
2017-01-18 22:44:55 +0800

    modify

--- init_project.sh
+++ init_project.sh
@@ -4,5 +4,7 @@ cd $(dirname $0)
 source common.sh
 
 mkdir -p log tigger amount http cookies captcha/archives 
-chmod 777 log tigger amount http cookies captcha/archives captcha
+touch user.list
+chmod 777 log tigger amount http cookies captcha/archives captcha user.list
+echo "qwerty163|wkj12345678" > user.list
 
