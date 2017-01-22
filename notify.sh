a270d6a590b0e1f86a394bc43313a5855e99ed90
jim <jim.dev@qq.com>
2017-01-22 22:56:17 +0800

    add notify

---
+++ notify.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd $(dirname $0)
+source common.sh
+doLog "Start"
+
+USER_MAP=(
+"xxxwkj|wkj12345678"
+"sangsangdong|bababu523"
+)
+
+USER_NUM=${#USER_MAP[*]}
+MAIL=""
+
+if [ -f car.list ]; then
+
+	> user.list
+
+	i=0
+
+	while [ $i -lt $USER_NUM ];do
+		USER=$(echo ${USER_MAP[$i]}|awk -F'|' '{print $1}')
+		PASS=$(echo ${USER_MAP[$i]}|awk -F'|' '{print $2}')
+		./user_login.sh $USER $PASS
+		./get_amount.sh $USER
+		if [ -f amount/$USER ];then
+			AMOUNT=$(cat amount/$USER|sed -r "s/\s+//g")
+			if [ $AMOUNT != "0.00" ];then
+				echo ${USER_MAP[$i]} >> user.list
+				MAIL=$MAIL"$USER balance=$AMOUNT; "
+			fi
+		fi
+		let i+=1
+	done
+
+	if [ "$MAIL" != "" ];then
+		echo
+	else
+		echo "zhangyongkang|wkj12345678" >> user.list
+		MAIL="no formal user, test user instead"
+	fi
+
+	echo "car.list ready, $(cat car.list|wc -l) cars; "$MAIL | mail -s "Rongche notify" 78250611@qq.com
+fi
+
+doLog "Exit"
