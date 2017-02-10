83c52c6be141db7247a0b922fe5fe6c4afcde4e6
jim <jim.dev@qq.com>
2017-02-10 16:08:54 +0800

    opt notify order

--- notify_response.sh
+++ notify_response.sh
@@ -7,7 +7,7 @@ doLog "Start"
 LOCAL_IP=$(/sbin/ifconfig eth1|grep inet|sed "s/:/ /g"|awk '{print $3}')
 
 if [ -f car.list ]; then
-	cat log/$(date +%Y%m%d)|grep -i valspeed|grep -i response|mail -s "[Rongche notify]Response - from $LOCAL_IP" 78250611@qq.com 
+	tac log/$(date +%Y%m%d)|grep -i valspeed|grep -i response|mail -s "[Rongche notify]Response - from $LOCAL_IP" 78250611@qq.com 
 fi
 
 doLog "Exit"
