c37629b873e19d93d0a11ded1cbae5a0a007527c
jim <jim.dev@qq.com>
2017-02-18 11:40:02 +0800

    opt

--- init_project.sh
+++ init_project.sh
@@ -9,3 +9,8 @@ chmod 777 log tigger amount http cookies captcha/archives captcha user.list
 echo "qwerty163|wkj12345678" > user.list
 echo "2878|/Info/T493000657/Front/InsideTwo/InsideTwo.aspx?Id=0CFA5440348BDE13" > car.list.debug
 
+mount.cifs //115.28.137.179/rongche_tigger /usr/local/apps/rongche/tigger -o user=root
+
+apt-get install samba
+apt-get install smbfs
+apt-get install tesseract-ocr 
