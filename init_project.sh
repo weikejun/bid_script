278f4cb980cae44fb1e1714b34aff388c755fd00
jim <jim.dev@qq.com>
2017-02-18 11:48:55 +0800

    opt

--- init_project.sh
+++ init_project.sh
@@ -3,14 +3,22 @@
 cd $(dirname $0)
 source common.sh
 
+# set dirs and files
 mkdir -p log tigger amount http cookies captcha/archives 
 touch user.list
 chmod 777 log tigger amount http cookies captcha/archives captcha user.list
 echo "qwerty163|wkj12345678" > user.list
 echo "2878|/Info/T493000657/Front/InsideTwo/InsideTwo.aspx?Id=0CFA5440348BDE13" > car.list.debug
 
-mount.cifs //115.28.137.179/rongche_tigger /usr/local/apps/rongche/tigger -o user=root
+# set crontab
+crontab -l > /tmp/cron.tmp
+cat crontab.dat >> /tmp/cron.tmp
+crontab /tmp/cron.tmp
 
+# set dependences
 apt-get install samba
 apt-get install smbfs
 apt-get install tesseract-ocr 
+
+mount.cifs //115.28.137.179/rongche_tigger /usr/local/apps/rongche/tigger -o user=root
+
