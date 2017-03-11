021ca4ddd2259d82182bf714b35e1ca39a67645d
jim <jim.dev@qq.com>
2017-03-11 22:50:22 +0800

    update

--- captcha/input.php
+++ captcha/input.php
@@ -32,7 +32,7 @@
 </div>
 <div class="am-g">
   <div class="am-u-lg-6 am-u-md-8 am-u-sm-centered">
-    <h3>验证码录入(时间:<span id="now_time">--:--:--</span>&nbsp;&nbsp;提交倒计时:<span id="countdown">10</span>)</h3>
+    <h3>验证码录入(时间:<span id="now_time">--:--:--</span>&nbsp;&nbsp;提交倒计时:<span id="countdown">15</span>)</h3>
     <hr>
     <form id="sub_form" method="post" class="am-form" action="submit.php">
       <div id="inputs"></div>
