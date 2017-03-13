e9b8e680f39d3db973f90bf9f2da11fbd9e9aa96
jim <jim.dev@qq.com>
2017-03-13 18:04:17 +0800

    update listen strategy

--- create_listeners.sh
+++ create_listeners.sh
@@ -4,28 +4,31 @@ cd $(dirname $0)
 source common.sh
 doLog "Start"
 
-if [ ! -f car.list ];then
+LIST_FILE="car.list"
+
+if [ ! -f $LIST_FILE ];then
 	doLog 'car.list not exists'
 	exit
 fi
 
-CAR_NO=$(echo $(cat car.list|wc -l)|awk '{srand();print (int(rand()*10))%$1;}')
-IND=-1
+declare -A DISPATCH
 
 doLog "Create listeners"
-for CAR_ID in $(cat car.list|awk -F"|" '{print $1}');do
-	IND=$[$IND + 1]
-	if [ $IND -ne $CAR_NO ];then
-		continue;
-	fi
+for CAR_ID in $(cat $LIST_FILE|awk -F"|" '{print $1}');do
 	for USER in $(cat user.list|sed -r "s/\s+//g");do
 		NAME=$(echo $USER|awk -F"|" '{print $1}')
+		if [ "${DISPATCH[$NAME]}" != "" ];then
+			continue;
+		fi
 		PASS=$(echo $USER|awk -F"|" '{print $2}')
 		./user_login.sh $NAME $PASS
 		./get_amount.sh $NAME
 		FILE_NAME=$(ls cookies/|grep $NAME|tail -n 1)
 		COOKIE_FILE="cookies/$FILE_NAME"
 		./process_bid.sh $FILE_NAME $CAR_ID &
+		doLog "Dispatch car=$CAR_ID to user=$NAME"
+		DISPATCH[$NAME]=$CAR_ID
+		break
 	done
 done
 
