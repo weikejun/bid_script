f8d7f6f99cd9a663cee4b4a5e95363bd603ced69
jim <jim.dev@qq.com>
2017-06-14 12:19:42 +0800

    up

--- get_car_list.sh
+++ get_car_list.sh
@@ -15,8 +15,23 @@ doLog "TouZi.aspx request, get car's list"
 curl -b "ItDoor=xiaolin;" -b $COOKIE_FILE 'http://www.zhongchoucar.com/Info/T493000657/Front/TouZi/TouZi.aspx?jc=zc' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: zh-CN,zh;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' --compressed -i > http/get_seed.http
 doLog "TouZi.aspx response"
 
-[ -f $LIST_FILE ] && rm $LIST_FILE
+if [ -f $LIST_FILE ]; then 
+	LASTMO=$(date -d "$(stat $LIST_FILE|grep -i "modify"|sed -r "s/modify:\s+//ig")" +%s)
+	LASTMO=$(($LASTMO + 3600))
+	if [ $(date +%s) -gt $LASTMO ];then
+		CMD="rm -f $LIST_FILE"
+		echo $CMD
+		eval $CMD
+	fi
+fi
 for uri in $(cat http/get_seed.http |grep -i "InsideTwo.aspx?Id"|sed -e "s/[<>]/\n/g"|grep "InsideTwo.aspx"|grep -e "^a href"|awk -F'"' '{print $2}'|uniq|xargs);do
+	if [ -f $LIST_FILE ]; then
+		EX=$(grep "$uri" $LIST_FILE|wc -l)
+		if [ "$EX" -ge 1 ];then
+			doLog "InsideTwo.aspx response, uri=$uri has exist"
+			continue
+		fi
+	fi
 	doLog "InsideTwo.aspx request, get car_id, uri=$uri"
 	CARID=$(curl -b "ItDoor=xiaolin;" -b $COOKIE_FILE  "http://www.zhongchoucar.com$uri" -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: zh-CN,zh;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Referer: http://www.zhongchoucar.com/Info/T493000657/Front/TouZi/TouZi.aspx?jc=zc' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed|grep "pro_title"|grep -e "id="|awk -F"'" '{print $4}')
 	echo "$CARID|$uri" >> $LIST_FILE 
